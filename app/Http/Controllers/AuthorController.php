75859aeecce02bcc78f3b74876d8ab9e43b92dd5
Icaa <92012266+ZahrunNafisah05@users.noreply.github.com>
2022-11-01 18:23:25 +0700

    challenge-author

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Book;
+use App\Models\Author;
+use PharIo\Manifest\Author as ManifestAuthor;
 
-
-class BookController extends Controller
+class AuthorController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -15,8 +15,8 @@ class BookController extends Controller
      */
     public function index()
     {
-        $book = Book::all();
-        return $book;
+        $author = Author::all();
+        return $author;
     }
 
     /**
@@ -37,12 +37,13 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        $table = Book::create([
-            "title" => $request->title,
-            "description" => $request->description,
-            "author" => $request->author,
-            "publisher" => $request->publisher,
-            "date_of_issue" => $request->date_of_issue
+        $table = Author::create([
+            "name" => $request->name,
+            "date_of_birth" => $request->date_of_birth,
+            "place_of_birth" => $request->place_of_birth,
+            "gender" => $request->gender,
+            "email" => $request->email,
+            "hp" => $request->hp
         ]);
 
         return response() ->json([
@@ -60,11 +61,11 @@ class BookController extends Controller
      */
     public function show($id)
     {
-        $book = book::find($id);
-        if ($book) {
+        $author = Author::find($id);
+        if ($author) {
             return response()->json([
                 'status' => 200,
-                'data' => $book
+                'data' => $author
 
             ], 200);
         }else {
@@ -95,17 +96,19 @@ class BookController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $book = book::find($id);
-        if($book){
-            $book->title = $request->title ? $request->title : $book->title; //tnfa tanya sm dgn if
-            $book->description = $request->description ? $request->description : $book->description;
-            $book->author = $request->author ? $request->author : $book->author;
-            $book->publisher = $request->publisher ? $request->publisher : $book->publisher;
-            $book->date_of_issue = $request->date_of_issue ? $request->date_of_issue : $book->date_of_issue;
-            $book->save();
+        $author = author::find($id);
+        if($author){
+            $author->name = $request->name ? $request->name : $author->name; //tnfa tanya sm dgn if
+            $author->date_of_birth = $request->date_of_birth ? $request->date_of_birth : $author->date_of_birth;
+            $author->place_of_birth = $request->place_of_birth ? $request->place_of_birth : $author->place_of_birth;
+            $author->gender = $request->gender ? $request->gender : $author->gender;
+            $author->email = $request->email ? $request->email : $author->email;
+            $author->hp = $request->hp ? $request->hp : $author->hp;
+            
+            $author->save();
             return response()->json([
                 'status' => 200,
-                'data' => $book
+                'data' => $author
             ], 200);
         }else{
             return response()->json([
@@ -123,12 +126,12 @@ class BookController extends Controller
      */
     public function destroy($id)
     {
-        $book = book::where('id', $id)->first();
-        if($book){
-            $book->delete();
+        $author = Author::where('id', $id)->first();
+        if($author){
+            $author->delete();
             return response()->json([
                 'status' =>200,
-                'data' => $book
+                'data' => $author
             ], 200);
         }else{
             return response()->json([
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,7 +28,10 @@ Route::post('/books', [BookController::class, 'store']);
 Route::put('/books/{id}', [BookController::class, 'update']);
 Route::delete('/books/{id}', [BookController::class, 'destroy']);
 
-
+Route::resource(
+    'authors', AuthorController::class)->except(
+        ['create','edit']
+    );
 
 
 
